e21a561fb187cedef2226bd1e34ad8ba0891f132
MezziNour <134057629+MezziNour@users.noreply.github.com>
2025-04-21 17:22:01 +0200

    com

--- HandiMap/public/js/avis.js
+++ HandiMap/public/js/avis.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   infoElements.forEach(function(infoElement) {
     infoElement.addEventListener('click', function() {
       const feedbackForm = infoElement.querySelector('.feedback-form');
-      // Toggle visibility of the feedback form
+      
       if (feedbackForm.style.display === 'none' || feedbackForm.style.display === '') {
         feedbackForm.style.display = 'block';
       } else {
